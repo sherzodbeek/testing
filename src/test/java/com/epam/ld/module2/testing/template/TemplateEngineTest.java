b9f0b3379e81ec1c5a618937c3b9a9e34e13d29d
Sherzod Omonov <sherzodbeek98@gmail.com>
2022-10-09 20:52:00 +0500

    1st test fail

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,16 @@
 package com.epam.ld.module2.testing.template;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class TemplateEngineTest {
+
+
+    @Test
+    void shouldGenerateMessage() {
+        fail();
+    }
+
+
 }
