cf41e17decf869c9c90169a89fa687420a8e4a90
Sherzod Omonov <sherzodbeek98@gmail.com>
2022-10-11 15:16:41 +0500

    messenger test passed

--- src/test/java/com/epam/ld/module2/testing/MessengerTest.java
+++ src/test/java/com/epam/ld/module2/testing/MessengerTest.java
@@ -1,12 +1,20 @@
 package com.epam.ld.module2.testing;
 
+import com.epam.ld.module2.testing.template.Template;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class MessengerTest {
 
     @Test
     void shouldSendMessage() {
-        fail();
+        Messenger messenger = mock(Messenger.class);
+        doNothing().when(messenger).sendMessage(any(Client.class), any(Template.class));
+
+        messenger.sendMessage(new Client(), new Template());
+
+        verify(messenger).sendMessage(any(Client.class), any(Template.class));
     }
 }
