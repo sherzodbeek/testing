948d88713489741a89348139244ee647eb5b62db
Sherzod Omonov <sherzodbeek98@gmail.com>
2022-10-12 14:13:54 +0500

    application console mode failed

---
+++ src/test/java/com/epam/ld/module2/testing/MessengerApplicationTest.java
@@ -0,0 +1,16 @@
+package com.epam.ld.module2.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MessengerApplicationTest {
+
+
+    @Test
+    void shouldStartConsoleMode() {
+        fail();
+    }
+
+}
